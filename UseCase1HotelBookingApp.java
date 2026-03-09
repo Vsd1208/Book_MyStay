ab95fcdc620af02f5928faa363ce8640777935f9
Vsd1208 <saidhirajvandanapu@gmail.com>
2026-03-09 09:19:43 +0530

    UseCase5

--- UseCase1HotelBookingApp.java
+++
@@ -1,9 +0,0 @@
-public class UseCase1HotelBookingApp {
-    public static void main(String[] args) {
-        System.out.println("====================================");
-        System.out.println("   Welcome to Hotel Booking System  ");
-        System.out.println("====================================");
-        System.out.println("Application started successfully.");
-
-    }
-}
